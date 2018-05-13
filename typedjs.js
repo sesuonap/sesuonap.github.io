9b2831b92a0e7fdbc27eba751cb94daed97b8bf0
sesuonap <tpanouses@gmail.com>
2018-05-13 15:10:26 -0500

    initial commit

--- typedjs.js
+++ typedjs.js
@@ -1,8 +1,34 @@
-import Typed from 'typed.js';
+// import Typed from 'typed.js';
 
-var options = {
-    strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
-    typeSpeed: 40
-}
+// var typed = new Typed('.element', {
+//     strings: ["First sentence.", "Second sentence."],
+//     typeSpeed: 30
+// });
 
-var typed = new Typed(".element", options);
+var originalMessage = document.querySelector('#greeting').innerHTML;
+var typedElement = document.querySelector('#greeting');
+var originalLength = typedElement.innerHTML.length;
+var i = originalLength - 1;
+var newMessage = "How can I help you?";
+var typeSpeed = 200;
+
+var deleteInterval = setInterval(function() {
+  typedElement.innerHTML = originalMessage.substring(0,i);
+  i--;
+  if (i <= 0) {
+    clearInterval(deleteInterval);
+  }
+}, typeSpeed);
+
+setTimeout(function() {
+  typedIndex = newMessage.length;
+
+  var addInterval = setInterval(function() {
+    // var originalMessage = document.querySelector('#greeting').innerHTML;
+    typedElement.innerHTML = newMessage.substring(0,i);
+    i++;
+    if (i > newMessage.length ) {
+      clearInterval(addInterval);
+    }
+  }, typeSpeed);
+}, originalLength * typeSpeed);
