7a274349ab4d7407f1232178f97cce300e914e37
sesuonap <tpanouses@gmail.com>
2018-05-15 15:11:00 -0500

    added insta/medium links

--- typedjs.js
+++ typedjs.js
@@ -1,9 +1,4 @@
-// import Typed from 'typed.js';
 
-// var typed = new Typed('.element', {
-//     strings: ["First sentence.", "Second sentence."],
-//     typeSpeed: 30
-// });
 
 var originalMessage = document.querySelector('#greeting').innerHTML;
 var typedElement = document.querySelector('#greeting');
@@ -12,6 +7,8 @@ var i = originalLength - 1;
 var newMessage = "How can I help you?";
 var typeSpeed = 200;
 
+
+//start with 'hello' and delete down from there to 'h'
 var deleteInterval = setInterval(function() {
   typedElement.innerHTML = originalMessage.substring(0,i);
   i--;
